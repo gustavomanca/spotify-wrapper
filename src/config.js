67cab9d126259ac2775f1378cab483aa3d4df565
Gustavo Manca <gustavo.manca@fatec.sp.gov.br>
2020-09-09 22:13:39 -0300

    refactor: rename lib name

--- src/config.js
+++ src/config.js
@@ -1,10 +1,3 @@
-const TOKEN_API =
-  'BQBMfiDVRYWKoVuO9zNWRYxKUEXygSZKq9fUTx7gG1ZrST4Y-0yf3LbJ_LHZl2GAjaXICX3jirxipfo0e8_hwh0dF9YSoURmwCz4dbIbN1rzOsfdhzspPcuh9AjJ6sCJpslUNSS4quhCoQ';
+const API_URL = 'https://api.spotify.com/v1';
 
-export const API_URL = 'https://api.spotify.com/v1';
-
-export const HEADERS = {
-  headers: {
-    Authorizarion: `Bearer ${TOKEN_API}`,
-  },
-};
+export default API_URL;
